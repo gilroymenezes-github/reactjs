07c122ecc66caee9222a4d353187270e57151dce
gilroymenezes-github <gilroymenezes@hotmail.com>
2021-10-12 16:50:22 +0530

    renamed chart to linechart

--- next-pn/pages/index.js
+++ next-pn/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
-import { LineChart } from '../components/Chart'
+import { LineChart } from '../components/linechart'
 import styles from '../styles/Home.module.css'
 
 
